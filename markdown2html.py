14faaca4c764b8b59d4a5bbd0a8d32b8ba2bc5e5
Pizzayolow <ralliard.valentin@gmail.com>
2023-12-11 10:30:22 +0100

    add if __name__ == "__main__":

--- markdown2html.py
+++ markdown2html.py
@@ -7,12 +7,13 @@ Second argument is the output file name"""
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
-    
-if not os.path.exists(sys.argv[1]):
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    exit(1)
-    
-exit(0)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+        
+    if not os.path.exists(sys.argv[1]):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        exit(1)
+        
+    exit(0)
