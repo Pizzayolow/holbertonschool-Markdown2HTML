8d0fb63416d29cbac114fea3276a7d39be46ecf8
Pizzayolow <ralliard.valentin@gmail.com>
2023-12-11 15:41:49 +0100

    task 1, markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,7 @@ Second argument is the output file name"""
 
 import sys
 import os
+import re
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -16,4 +17,28 @@ if __name__ == "__main__":
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         exit(1)
         
-    exit(0)
+    readme = sys.argv[1]
+    readhtml = sys.argv[2]
+    count = 0
+    
+    # Définir une expression régulière pour trouver les titres
+    with open(readme, 'r', encoding='utf-8') as readme:
+            lines = readme.readlines()
+            print(lines)
+            with open(readhtml, 'w', encoding='utf-8') as tohtml:
+                for line in lines:
+                    print(line)
+                    count=0
+                    for char in line:
+                        if char == '#':
+                            count += 1
+            
+        
+                    if count > 0:
+                            tohtml.write(f"<h{count}>{line.strip('#').strip()}</h{count}>\n")
+
+
+
+
+
+
